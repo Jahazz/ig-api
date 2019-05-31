735e5e0305f0d496c1b3b515aabb0ed1bf94fa8d
Jakub Dyja <jakub.dyja@gmail.com>
2019-05-31 22:53:56 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,82 +1,113 @@
-import { create, setHeaderTokens } from './axios'
-import {
-  uniqueId,
-  getOption,
-  isFunction,
-  publicEncrypt,
-  transformError,
-  transformResponse
-} from './utils'
+'use strict';
 
-export default class IG {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
-  static transformResponse = transformResponse
-  static transformError = transformError
-  static uniqueId = uniqueId
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-  constructor(apiKey, isDemo, options) {
-    this.api = create(apiKey, isDemo)
+var _axios = require('./axios');
+
+var _utils = require('./utils');
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var IG = function () {
+  function IG(apiKey, isDemo, options) {
+    _classCallCheck(this, IG);
+
+    this.api = (0, _axios.create)(apiKey, isDemo);
     this.defaults = Object.assign({
-      transformResponse,
-      transformError
-    }, options)
+      transformResponse: _utils.transformResponse,
+      transformError: _utils.transformError
+    }, options);
   }
 
-  request(method, path, version, config, options) {
-    const transformRes = getOption('transformResponse', options, this.defaults)
-    const transformErr = getOption('transformError', options, this.defaults)
+  _createClass(IG, [{
+    key: 'request',
+    value: function request(method, path, version, config, options) {
+      var transformRes = (0, _utils.getOption)('transformResponse', options, this.defaults);
+      var transformErr = (0, _utils.getOption)('transformError', options, this.defaults);
+      var request;
+      if(method != "delete")
+        request = this.api.request(Object.assign({}, config, {
+        method: method, url: path, headers: { Version: version || 1 }
+      }));
+      else
+        request = this.api.request(Object.assign({}, config, {
+          method: "post", url: path, headers:
+              {
+                Version: version || 1,
+                _method: "DELETE"
+              }
+        }));
 
-    let request = this.api.request(Object.assign({}, config, {
-      method, url: path, headers: { Version: version || 1 }
-    }))
+      if ((0, _utils.isFunction)(transformRes)) request = request.then(transformRes);
+      if ((0, _utils.isFunction)(transformErr)) request = request.catch(transformErr);
+      return request;
+    }
+  }, {
+    key: 'get',
+    value: function get(path, version, params, options) {
+      return this.request('get', path, version, { params: params }, options);
+    }
+  }, {
+    key: 'post',
+    value: function post(path, version, data, options) {
 
-    if (isFunction(transformRes)) request = request.then(transformRes)
-    if (isFunction(transformErr)) request = request.catch(transformErr)
+      return this.request('post', path, version, { data: data }, options);
+    }
+  }, {
+    key: 'put',
+    value: function put(path, version, data, options) {
+      return this.request('put', path, version, { data: data }, options);
+    }
+  }, {
+    key: 'delete',
+    value: function _delete(path, version, data, options) {
+      return this.request('delete', path, version, { data: data }, options);
+    }
+  }, {
+    key: 'login',
+    value: function login(username, password, encryptPassword, options) {
+      var _this = this;
 
-    return request
-  }
+      var encryptedPassword = encryptPassword === true;
+      var processPassword = encryptedPassword ? this.get('session/encryptionKey', 1, null, {
+        transformResponse: _utils.transformResponse
+      }).then(function (_ref) {
+        var encryptionKey = _ref.encryptionKey,
+            timeStamp = _ref.timeStamp;
 
-  get(path, version, params, options) {
-    return this.request('get', path, version, { params }, options)
-  }
+        return (0, _utils.publicEncrypt)(encryptionKey, password + '|' + timeStamp);
+      }) : Promise.resolve(password);
 
-  post(path, version, data, options) {
-    return this.request('post', path, version, { data }, options)
-  }
+      return processPassword.then(function (result) {
+        return _this.post('session', 2, {
+          encryptedPassword: encryptedPassword,
+          identifier: username,
+          password: result
+        }, {
+          transformResponse: false
+        }).then(function (response) {
+          (0, _axios.setHeaderTokens)(_this.api, response);
+          var transformRes = (0, _utils.getOption)('transformResponse', options, _this.defaults);
+          return (0, _utils.isFunction)(transformRes) ? transformRes(response) : response;
+        });
+      });
+    }
+  }, {
+    key: 'logout',
+    value: function logout(options) {
+      return this.delete('session', 1, null, options);
+    }
+  }]);
 
-  put(path, version, data, options) {
-    return this.request('put', path, version, { data }, options)
-  }
+  return IG;
+}();
 
-  delete(path, version, data, options) {
-    return this.request('delete', path, version, { data }, options)
-  }
-
-  login(username, password, encryptPassword, options) {
-    const encryptedPassword = encryptPassword === true
-    const processPassword = encryptedPassword ?
-      this.get('session/encryptionKey', 1, null, {
-        transformResponse: transformResponse
-      }).then(({ encryptionKey, timeStamp }) => {
-        return publicEncrypt(encryptionKey, `${password}|${timeStamp}`)
-      }) : Promise.resolve(password)
-
-    return processPassword.then((result) => {
-      return this.post('session', 2, {
-        encryptedPassword,
-        identifier: username,
-        password: result
-      }, {
-        transformResponse: false
-      }).then((response) => {
-        setHeaderTokens(this.api, response)
-        const transformRes = getOption('transformResponse', options, this.defaults)
-        return isFunction(transformRes) ? transformRes(response) : response
-      })
-    })
-  }
-
-  logout(options) {
-    return this.delete('session', 1, null, options)
-  }
-}
+IG.transformResponse = _utils.transformResponse;
+IG.transformError = _utils.transformError;
+IG.uniqueId = _utils.uniqueId;
+exports.default = IG;
+module.exports = exports['default'];
